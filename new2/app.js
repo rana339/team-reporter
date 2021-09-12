fefa32ef196f1a58057eb5f539f336695241b523
Rana Ameer <ameerrana141@gmail.com>
2021-09-12 16:26:35 +0500

    my

--- new2/app.js
+++ new2/app.js
@@ -3,14 +3,14 @@ function signup() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    let value = false;
+    let num = false;
     for (let i = 0; i < users.length; i++) {
         if (email === users[i].email) {
-            value = true;
-            alert("email already register");
+            num = true;
+            alert("this email is register. please enter another email");
         }
     }
-    if (value === false) {
+    if (num === false) {
         let data = [];
         data.push({ name, email, password })
         let alluser = [...data, ...users]
@@ -24,34 +24,24 @@ function login() {
 
 
     let user = JSON.parse(localStorage.getItem("user"));
-    let val = false;
+    let value = false;
     for (let i = 0; i < user.length; i++) {
         if (mail === user[i].email && pass === user[i].password) {
-            val = true;
-            window.location.href = "index.html"
+            value = true;
+            window.location.href = "c.html"
         }
     }
-    if (val === false) {
-        alert("invalid email or password");
+    if (value === false) {
+        alert("please enter a valid email and password");
     }
 }
 
-// document.getElementById("create_team").style.display = "none";
-
-function createteam() {
-    document.getElementById("create_team").style.display = "block";
-}
-
-document.getElementById("child_container").style.display = "none";
-
 function create() {
-    let all_data = JSON.parse(localStorage.getItem("data")) || [];
     document.getElementById("child_container").style.display = "block";
     document.getElementById("create_team").style.display = "none";
     let create_team = document.getElementById("my_team");
     let member_name = document.getElementById("member_name").value;
     let team_name = document.getElementById("team_name").value;
-    let person_email = document.getElementById("person_email").value;
     let div = document.createElement("div");
     div.classList.add("div-text");
     let team_Name = document.createTextNode(team_name);
@@ -61,13 +51,17 @@ function create() {
     div.appendChild(space);
     div.appendChild(team_member);
     create_team.appendChild(div);
-    let all = document.getElementsByClassName("div-text");
-    let data = [];
-    data.push({all})
-    localStorage.setItem('data', JSON.stringify(data));  
-
 }
 
+
+// document.getElementById("create_team").style.display = "none";
+
+document.getElementById("child_container").style.display = "none";
+
 function cancel() {
-    window.location.href = "index.html"
+    window.location.href = "c.html"
+}
+
+function createteam() {
+    document.getElementById("create_team").style.display = "block";
 }
